d6a7d27b9a241b9bcf2d0312037350854b478d64
e3oroush <ebrahim.soroush@gmail.com>
2019-02-09 23:06:30 +0330

    Messing with keras and pytorch

---
+++ models.py
@@ -0,0 +1,101 @@
+from utils import DinoSeleniumEnv, show_image, IMAGE_CHANNELS, IMAGE_WIDTH, IMAGE_HEIGHT
+import torch
+from torch import nn
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.optimizers import Adam
+from keras.models import Sequential
+
+class Agent(object):
+    def __init__(self, env): # takes dino game environment as input
+        self._env = env
+        self._jump() # start the game with a jump
+        self.actions = [self._nothing, self._jump, self._duck]
+
+    def _jump(self):
+        self._env.press_up()
+    
+    def _duck(self):
+        self._env.press_down()
+    
+    def _nothing(self):
+        pass # do nothing
+    
+    def perform_action(self, action_indx):
+        self.actions[action_indx]()
+
+    
+    def is_crashed(self):
+        return self._env.is_crashed()
+
+DEFAULT_REWARD=0.1
+GAMEOVER_PUNISHMENT=-1
+class GameState(object):
+    def __init__(self, agent, debug=True):
+        self._agent = agent
+        self._display = None
+        if debug:
+            self._display = show_image() # a coroutine method
+            self._display.__next__()
+    def get_state(self, action_indx):
+        score = self._agent._env.get_score()
+        reward = DEFAULT_REWARD
+        is_gameover = False
+        self._agent.perform_action(action_indx)
+        screen = self._agent._env.grab_screen()
+        if self._display is not None:
+            self._display.send(screen)
+        if self._agent._env.is_crashed():
+            is_gameover = True
+            reward = GAMEOVER_PUNISHMENT
+            self._agent._env.restart_game()
+        return screen, reward, is_gameover, score
+
+class FlattenTorch(nn.Module):
+    def forward(self,x):
+        return x.view(x.shape[0], -1)
+
+class QNetwork(nn.Module):
+    def __init__(self, nb_actions, hidden_size=512):
+        super(QNetwork, self).__init__()
+        self.image_encoder = nn.Sequential(nn.Conv2d(IMAGE_CHANNELS, 32, 7, stride=4, padding=2),
+                                            nn.MaxPool2d(2),
+                                            nn.ReLU(),
+                                            nn.Conv2d(32,64,4,stride=2,padding=1),
+                                            nn.MaxPool2d(2),
+                                            nn.ReLU(),
+                                            nn.Conv2d(64,64,3,stride=1,padding=1),
+                                            nn.MaxPool2d(2),
+                                            nn.ReLU(),
+                                            FlattenTorch())
+        self.q_value_estimator = nn.Sequential(nn.Linear(64,hidden_size),
+                                                nn.ReLU(),
+                                                nn.Linear(hidden_size,nb_actions))
+    
+    def forward(self, image):
+        if next(self.parameters()).is_cuda:
+            image = image.cuda()
+        encoded_features = self.image_encoder(image)
+        return self.q_value_estimator(encoded_features)
+
+
+def build_keras_model():
+    print("Now we build the model")
+    model = Sequential()
+    model.add(Conv2D(32, (8, 8), padding='same',strides=(4, 4),input_shape=(IMAGE_WIDTH,IMAGE_HEIGHT,IMAGE_CHANNELS)))  #80*80*4
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Activation('relu'))
+    model.add(Conv2D(64, (4, 4),strides=(2, 2),  padding='same'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Activation('relu'))
+    model.add(Conv2D(64, (3, 3),strides=(1, 1),  padding='same'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Activation('relu'))
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dense(2))
+    adam = Adam(lr=1e-4)
+    model.compile(loss='mse',optimizer=adam)
+    
+    return model
---
+++ train.py
@@ -0,0 +1,140 @@
+import torch
+from models import QNetwork, Agent, GameState, build_keras_model
+from utils import DinoSeleniumEnv,IMAGE_CHANNELS, IMAGE_WIDTH, IMAGE_HEIGHT, get_logger
+import numpy as np
+from collections import deque
+import time
+
+
+logger = get_logger("training", "training.log")
+def train():
+    chrome_driver_path = "/home/esoroush/github/chromedriver"
+    checkpoint_path = "model.pth"
+    nb_actions = 2
+    epsilon = 0.1
+    initial_epsilon = 0.1
+    final_epsilon = 1e-4
+    gamma = 0.99
+    nb_memory = 5000
+    nb_expolre = 100000
+    is_debug = False
+    batch_size = 32
+    nb_observation = 1000
+    seed = 22
+    is_cuda = True
+    np.random.seed(seed)
+    memory = deque()
+    env = DinoSeleniumEnv(chrome_driver_path)
+    agent = Agent(env)
+    game_state = GameState(agent, debug=is_debug)
+    qnetwork = build_keras_model()#QNetwork(nb_actions)
+    # if is_cuda:
+    #     qnetwork.cuda()
+    # optimizer = torch.optim.Adam(qnetwork.parameters())
+    # try:
+    #     m = torch.load(checkpoint_path)
+    #     qnetwork.load_state_dict(m['qnetwork'])
+    #     optimizer.load_state_dict(m['optimizer'])
+    # except:
+    #     logger.warn("No model found in {}".format(checkpoint_path))
+    # loss_fcn = torch.nn.MSELoss()
+    action_indx = 0 # do nothing as the first action
+    screen, reward, is_gameover, score = game_state.get_state(action_indx)
+    current_state = np.expand_dims(screen,0)
+    current_state = np.tile(current_state,(IMAGE_CHANNELS,1,1)) # [IMAGE_CHANNELS,IMAGE_WIDTH,IMAGE_HEIGHT]
+    initial_state = current_state
+
+    t = 0
+    last_time = 0
+    sum_scores = 0
+    total_loss = 0
+    max_score = 0
+    try:
+        while True:
+            # qnetwork.eval()
+            if np.random.random() < epsilon: # epsilon greedy
+                action_indx = np.random.randint(nb_actions)
+            else:
+                tensor = np.expand_dims(current_state.transpose(1,2,0),0)
+                qvalues = qnetwork.predict(tensor).squeeze()
+                action_indx = np.argmax(qvalues)
+                # tensor = torch.from_numpy(current_state).float().unsqueeze(0)
+                # with torch.no_grad():
+                #     qvalues = qnetwork(tensor).squeeze()
+                # _,action_indx = qvalues.max(-1)
+                # action_indx = action_indx.item()
+            if epsilon > final_epsilon and t > nb_observation:
+                epsilon -= (initial_epsilon - final_epsilon) / nb_expolre 
+            screen, reward, is_gameover, score = game_state.get_state(action_indx)
+            sum_scores += score
+            if score > max_score:
+                max_score = score
+            if last_time and t % 1000 == 0:
+                logger.info('fps: {0}'.format(1 / (time.time()-last_time))) 
+            last_time = time.time()
+            screen = np.expand_dims(screen,0)
+            next_state = np.append(screen,current_state[:IMAGE_CHANNELS-1,:,:], axis=0)
+            memory.append((current_state, action_indx, reward, next_state, is_gameover))
+            if len(memory) > nb_memory:
+                memory.popleft()
+            if t > nb_observation:
+                indxes = np.random.choice(len(memory), batch_size, replace=False)
+                minibatch = [memory[b] for b in indxes]
+                # inputs = np.zeros((batch_size,IMAGE_CHANNELS,IMAGE_WIDTH,IMAGE_HEIGHT))
+                inputs = np.zeros((batch_size,IMAGE_WIDTH,IMAGE_HEIGHT,IMAGE_CHANNELS))
+                targets = np.zeros((batch_size,nb_actions))
+                for i,(state_t,action_t,reward_t,state_t1,is_gameover_t1) in enumerate(minibatch):
+                    inputs[i] = state_t.transpose(1,2,0)
+                    tensor = np.expand_dims(state_t.transpose(1,2,0),0)
+                    qvalues = qnetwork.predict(tensor).squeeze()
+                    targets[i] = qvalues
+                    # tensor = torch.from_numpy(state_t).float().unsqueeze(0)
+                    # with torch.no_grad():
+                    #     qvalues = qnetwork(tensor).squeeze()
+                    # targets[i] = qvalues.cpu().numpy()
+                    if is_gameover_t1:
+                        targets[i,action_t] = reward_t
+                    else:
+                        # tensor = torch.from_numpy(state_t1).float().unsqueeze(0)
+                        # with torch.no_grad():
+                        #     qvalues = qnetwork(tensor).squeeze()
+                        #     qvalues = qvalues.cpu().numpy()
+                        tensor = np.expand_dims(state_t1.transpose(1,2,0),0)
+                        qvalues = qnetwork.predict(tensor).squeeze()
+                        targets[i,action_t] = reward_t + gamma * np.max(qvalues)
+                # qnetwork.train()
+                # qnetwork.zero_grad()
+                # inputs = torch.from_numpy(inputs).float()
+                # targets = torch.from_numpy(targets).float()
+                # if is_cuda:
+                #     targets = targets.cuda()
+                # qvalues = qnetwork(inputs)
+                # loss = loss_fcn(qvalues,targets)
+                # loss.backward()
+                # optimizer.step()
+                # total_loss += loss.item()
+                total_loss += qnetwork.train_on_batch(inputs, targets)
+            current_state = initial_state if is_gameover else next_state
+            env.restart_game()
+            t+=1
+            if t % 1000 == 0:
+                env.pause_game()
+                logger.info("For episode {}: mean score is {} max score is {} mean loss: {}".format(t, sum_scores/t, max_score, total_loss/t))
+                qnetwork.save_weights("model.h5")
+                # torch.save({"qnetwork":qnetwork.state_dict(),
+                #         "optimizer":optimizer.state_dict()},checkpoint_path)
+                env.resume_game()
+            if t%10 == 0:
+                logger.info("Episode: {} action_index: {} reward: {} max qvalue: {}".format(t,action_indx,reward,np.max(qvalues)))  
+    except KeyboardInterrupt:
+        qnetwork.save_weights("model.h5")
+        # torch.save({"qnetwork":qnetwork.state_dict(),
+        #             "optimizer":optimizer.state_dict()},checkpoint_path)
+
+if __name__ == '__main__':
+    train()
+
+
+
+
+
